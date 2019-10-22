706010695899b082fa4fca930ff27270a72ee3dc
Shawn Pyle <shawn.pyle@the-jci.org>
2019-10-22 15:38:00 -0400

    Throw error with useful information if we cannot eval a spec

--- test/run.jsx
+++ test/run.jsx
@@ -3,21 +3,30 @@
 logger.warn('To prevent odd errors, usually methods not being defined, login into Adobe Creative Cloud and all modal/dialog boxes are open. If tests are not running as expected, try restarting the application.');
 
 //load specs 
-var specPath = rootPath + '/test/spec';
+var specPath = new File(rootPath + '/test/spec');
 if (arguments && arguments.length > 0) { //load specified files
-	logger.info('Loading specs: ' + arguments.toString());
+	logger.info('Loading specs: ' + arguments.join(', '));
 	arguments.forEach(function (specName) {
 		if (!specName.match(/Spec/)) {
 			specName += 'Spec';
 		}
-		$.evalFile(specPath + '/' + specName + '.jsx');
+		var f = new File(specPath.fsName + '/' + specName + '.jsx');
+		try {
+			$.evalFile(f);
+		} catch(e) {
+			throw new Error('Failed to load '+f.fsName+' because '+e.message+' on line '+e.line+'.');
+		}
 	});
 } else { //load everything
-	logger.info('Loading all specs in ' + specPath.toString());
-	var specFolder = new Folder(specPath);
+	logger.info('Loading all specs in ' + specPath.fsName);
+	var specFolder = new Folder(specPath.fsName);
 	specFolder.getFiles().forEach(function (f) {
 		if (f.name.match(/Spec\.jsx$/)) {
-			$.evalFile(f.fullName);
+			try {
+				$.evalFile(f);
+			} catch(e) {
+				throw new Error('Failed to load '+f.fsName+' because '+e.message+' on line '+e.line+'.');
+			}
 		}
 	});
 }
